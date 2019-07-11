73e87cd2e800d26bb2dbc4441c856f27106aa9c7
王智鹏 <wangzhipeng@qinxue100.com>
2019-07-11 15:33:28 +0800

    更新

---
+++ QXQrCodeScan/QXQrCodeScan/QXQrCodeScan.h
@@ -0,0 +1,17 @@
+//
+//  QXQrCodeScan.h
+//  QXQrCodeScan
+//
+//  Created by mac on 2019/7/11.
+//  Copyright © 2019 zhihuiketang. All rights reserved.
+//
+
+#ifndef QXQrCodeScan_h
+#define QXQrCodeScan_h
+
+
+#import "QXImagePicker.h"
+#import "QXQrCodeScanVC.h"
+
+
+#endif /* QXQrCodeScan_h */
